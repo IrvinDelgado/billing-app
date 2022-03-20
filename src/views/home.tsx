1a78b4acff2a526350bcd2e39766e4930295f03f
Irvin Delgado <irvind21@gmail.com>
2022-03-20 11:48:38 -0500

    mock api

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,27 +1,48 @@
-import React from 'react';
-import { FlatList, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 import Bill from '../components/Bill';
 import DonutChart from '../components/DonutChart';
 import { BILLS } from '../DummyData/BILLS';
 import { IBill } from '../models/IBill';
 
-const renderItemComponent = (data:IBill) => <Bill bill={data}/>
-
-const ItemSeparator = () => <View style={{ height: 2 }}/>
-
 const Home = ({navigation}: any) => {
-  const logout = () => {
-    navigation.navigate('Home');
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<Array<IBill>>([]);
+
+  const renderItemComponent = (data:IBill) => <Bill bill={data}/>
+  const ItemSeparator = () => <View style={{ height: 2 }}/>
+
+
+  // Mock Fetch API
+  const getBills = () => {
+    try {
+      setTimeout(()=>{
+        const result = BILLS;
+        setData(result);
+        setLoading(false);
+      }, 1000);
+    } catch (error) {
+      console.error(error);
+    }
   }
+
+  useEffect(()=>{
+    getBills();
+  },[])
+
   return (
     <View>
-      <DonutChart data={BILLS}/>
-      <FlatList
-        data={BILLS}
-        renderItem={({item}) => renderItemComponent(item)}
-        ItemSeparatorComponent={ItemSeparator}
-        keyExtractor={(item)=>item.name}
-      />
+      {isLoading ? <ActivityIndicator size={"large"}/>:(
+        <View>
+        <DonutChart data={data}/>
+        <FlatList
+          data={data}
+          renderItem={({item}) => renderItemComponent(item)}
+          ItemSeparatorComponent={ItemSeparator}
+          keyExtractor={(item)=>item.name}
+        />
+        </View>
+      )}
     </View>
   );
 }
